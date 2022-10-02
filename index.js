71418ac9ec27f7acf0f0bd4ceb5fe05aad76b539
Gouse Basha <mohammadgous526@gmail.com>
2022-10-02 14:57:05 +0530

    Implemented Dynamic Data

--- index.js
+++ index.js
@@ -56,20 +56,24 @@ const data = fs.readFileSync("./dev-data/data.json", "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+
+  const {query, pathname} = url.parse(req.url, true);
 
   //Overview Page
-  if (pathName === "/" || pathName === "/overview") {
+  if (pathname === "/" || pathname === "/overview") {
     res.writeHead(200, { "Content-type": "text/html" });
 
     const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el)).join('');
      const output = tempOverview.replace('{%PRODUCTS_CARDS%}', cardsHtml)
     res.end(output);
     //Product Page
-  } else if (pathName === "/product") {
-    res.end("This is the PRODUCT");
+  } else if (pathname === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
     //API
-  } else if (pathName === "/api") {
+  } else if (pathname === "/api") {
     res.writeHead(200, { "Content-type": "application/json" });
     res.end(data);
     //Not Found
